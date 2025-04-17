03cdbd58c7860b086df4cc861aebea1014e7fe7e
Shreedhar Hegde <snhkelemane@gmail.com>
2025-04-17 22:32:03 +0530

    added link to settings page from about page

--- expo-learning/app/(tabs)/about.tsx
+++ expo-learning/app/(tabs)/about.tsx
@@ -1,21 +1,34 @@
-import {View,Text, StyleSheet} from "react-native"
+import { Link } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-export default function AboutScreen(){ 
- return  (
+export default function AboutScreen() {
+  return (
     <View style={styles.container}>
-        <Text style={styles.text}>About Screen</Text>
+      <Text style={styles.text}>About Screen</Text>
+      <Pressable style={styles.button}>
+        <Link href={"/settings"} style={{}}>
+          <Text style={styles.text}>Go to Settings</Text>
+        </Link>
+      </Pressable>
     </View>
- )
- }
+  );
+}
 
- const styles=StyleSheet.create({
-    container:{
-        flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
-    },
-    text:{
-        color:"#fff"
-    }
- }) 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#4067f5",
+    marginTop: 10,
+    borderRadius: 4,
+    paddingHorizontal: 16,
+  },
+});
