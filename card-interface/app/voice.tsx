136c91872fed1c8556b22f19c28d5d95273ffa3e
Shreedhar Hegde <snhkelemane@gmail.com>
2025-06-13 13:27:20 +0530

    feat(voice): enhance VoiceRecorder with playback controls
    - Added functionality to play sample music and control playback with stop and pause options.
    - Improved playback state management using a ref to track sound instances and playback position.
    - Updated UI to reflect playback status and provide user feedback during audio playback.

--- card-interface/app/voice.tsx
+++ card-interface/app/voice.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Pressable, Text } from "react-native";
 import {
   Audio,
   AVPlaybackStatusSuccess,
   InterruptionModeAndroid,
   InterruptionModeIOS,
+  AVPlaybackSource,
 } from "expo-av";
 import { Ionicons } from "@expo/vector-icons";
 import Animated, {
@@ -15,15 +16,19 @@ import Animated, {
   Easing,
   withSequence,
 } from "react-native-reanimated";
+import { transcribeAudio } from "@/api/voice-transcript";
 
 const startSound = require("@/assets/sounds/start.mp3");
 const stopSound = require("@/assets/sounds/stop.mp3");
+const music = require("@/assets/sounds/music.mp3");
 
 export default function VoiceRecorder() {
   const [recording, setRecording] = useState<Audio.Recording | null>(null);
   const [recordedUri, setRecordedUri] = useState<string | null>(null);
   const [sound, setSound] = useState<Audio.Sound | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const soundRef = useRef<Audio.Sound | null>(null);
+  const [playbackPosition, setPlaybackPosition] = useState<number>(0);
 
   // Animation values for waves
   const waveHeights = Array.from({ length: 5 }, () => useSharedValue(10));
@@ -86,7 +91,6 @@ export default function VoiceRecorder() {
     }
   }, [isPlaying]);
 
-  // 🔊 Utility to play a sound
   const playEffect = async (soundPath: any): Promise<void> => {
     return new Promise((resolve) => {
       Audio.Sound.createAsync(soundPath).then(({ sound }) => {
@@ -104,6 +108,7 @@ export default function VoiceRecorder() {
   const startRecording = async () => {
     try {
       const { granted } = await Audio.requestPermissionsAsync();
+      if (recording) return;
       if (granted) {
         await Audio.setAudioModeAsync({
           allowsRecordingIOS: true,
@@ -136,29 +141,84 @@ export default function VoiceRecorder() {
       const uri = recording.getURI();
       setRecordedUri(uri);
       setRecording(null);
-      alert(
-        `Recording successful.\nDuration : ${Math.floor(
-          durationMillis / 1000
-        )} s`
-      );
+      if (isDoneRecording) {
+        alert(
+          `Recording successful.\nDuration : ${Math.floor(
+            durationMillis / 1000
+          )} s`
+        );
+      }
       await playEffect(stopSound);
     } catch (err) {
       console.error("Failed to stop recording", err);
     }
   };
 
-  const playSound = async () => {
-    if (!recordedUri) return;
-    const { sound } = await Audio.Sound.createAsync({ uri: recordedUri });
-    setSound(sound);
+  const playSound = async (music?: AVPlaybackSource) => {
+    // if (!recordedUri) return;
+
+    // If playing already store the playback position and stop the music
+    if (soundRef.current && isPlaying) {
+      const { positionMillis } =
+        (await soundRef.current.getStatusAsync()) as AVPlaybackStatusSuccess;
+      await soundRef.current.stopAsync();
+      setPlaybackPosition(positionMillis);
+      // soundRef.current = null;
+      setIsPlaying(false);
+      // console.log("INFO =>", `STOP`, positionMillis);
+      return;
+    }
+
+    //  if not playing and was stopped playing between then start from where it stopped.
+    if (soundRef.current && !isPlaying) {
+      // console.log("INFO =>", `CONTINUE`, playbackPosition);
+      await soundRef.current.setStatusAsync({
+        positionMillis: playbackPosition,
+        shouldPlay: true,
+      });
+
+      setIsPlaying(true);
+      return;
+    }
+
+    // console.log("INFO =>", `play first time`);
+
+    // const { sound } = await Audio.Sound.createAsync({ uri: recordedUri });
+    let newSound: Audio.Sound;
+    if (!!music) {
+      const { sound } = await Audio.Sound.createAsync(music);
+      newSound = sound;
+    } else {
+      const { sound } = await Audio.Sound.createAsync({
+        uri: recordedUri as string,
+      });
+      newSound = sound;
+    }
+    soundRef.current = newSound;
+    setSound(newSound);
     setIsPlaying(true);
-    await sound.playAsync();
-    sound.setOnPlaybackStatusUpdate((status) => {
+    await newSound.playAsync();
+
+    newSound.setOnPlaybackStatusUpdate((status) => {
       if ((status as AVPlaybackStatusSuccess).didJustFinish) {
         setIsPlaying(false);
-        sound.unloadAsync();
+        newSound.unloadAsync();
+        soundRef.current = null;
       }
     });
+  };
+
+  const stopSoundPlay = async () => {
+    if (soundRef.current) {
+      await soundRef.current.stopAsync();
+      soundRef.current = null;
+      setIsPlaying(false);
+      setPlaybackPosition(0);
+    }
+  };
+
+  const handleAudioTranscribe = async () => {
+    await transcribeAudio(recordedUri as string);
   };
 
   const waveStyles = waveHeights.map((height, index) =>
@@ -173,8 +233,25 @@ export default function VoiceRecorder() {
   }));
 
   return (
-    <View className="flex-1 justify-center items-center p-5">
+    <View className="flex-1  p-5">
       {/* Status indicators in fixed position above buttons */}
+      <View
+        className={`flex-row p-4 justify-between ${
+          isPlaying ? "bg-slate-200 text-white" : ""
+        }`}
+      >
+        <Pressable onPress={() => playSound(music)} className="flex-1 ">
+          <Text> 1. Sample Music</Text>
+        </Pressable>
+        {isPlaying && (
+          <Pressable
+            onPress={stopSoundPlay}
+            className="w-[20px] h-[20px] rounded-full bg-red-500 justify-center items-center "
+          >
+            <Ionicons name="stop" size={10} color="white" />
+          </Pressable>
+        )}
+      </View>
       <View className="h-[40px] mb-4 justify-center items-center">
         {recording && (
           <View className="flex-row items-center gap-1">
@@ -188,17 +265,6 @@ export default function VoiceRecorder() {
             <Text className="text-blue-500 text-base ml-2">Recording...</Text>
           </View>
         )}
-        {isPlaying && !recording && (
-          <Animated.View
-            className="flex-row items-center gap-1"
-            style={playbackStyle}
-          >
-            <View className="w-[3px] h-[20px] bg-green-500 rounded-sm" />
-            <View className="w-[3px] h-[30px] bg-green-500 rounded-sm" />
-            <View className="w-[3px] h-[20px] bg-green-500 rounded-sm" />
-            <Text className="text-green-500 text-base ml-2">Playing...</Text>
-          </Animated.View>
-        )}
       </View>
 
       {/* Buttons in fixed position below */}
@@ -211,14 +277,41 @@ export default function VoiceRecorder() {
         >
           <Ionicons name={recording ? "stop" : "mic"} size={24} color="white" />
         </Pressable>
-        {recordedUri && !recording && (
+      </View>
+      <View className="flex gap-2 absolute bottom-0 w-full left-0  p-4 mb-4">
+        {isPlaying && !recording && (
+          <Animated.View
+            className="flex-row items-center gap-1 justify-center p-2"
+            style={playbackStyle}
+          >
+            <View className="w-[3px] h-[20px] bg-green-500 rounded-sm" />
+            <View className="w-[3px] h-[30px] bg-green-500 rounded-sm" />
+            <View className="w-[3px] h-[20px] bg-green-500 rounded-sm" />
+            <Text className="text-green-500 text-base ml-2">Playing...</Text>
+          </Animated.View>
+        )}
+        <View className=" flex-row justify-center gap-2">
           <Pressable
             className="w-[50px] h-[50px] rounded-full bg-green-500 justify-center items-center"
-            onPress={playSound}
+            onPress={() => playSound()}
           >
-            <Ionicons name="play" size={24} color="white" />
+            {isPlaying ? (
+              <Ionicons name="pause" size={24} color="white" />
+            ) : (
+              <Ionicons name="play" size={24} color="white" />
+            )}
           </Pressable>
-        )}
+          {
+            <Pressable
+              className={`w-[50px] h-[50px] rounded-full bg-red-500 justify-center items-center  ${
+                isPlaying || playbackPosition > 0 ? "block" : "invisible"
+              }`}
+              onPress={stopSoundPlay}
+            >
+              <Ionicons name="stop" size={24} color="white" />
+            </Pressable>
+          }
+        </View>
       </View>
     </View>
   );
