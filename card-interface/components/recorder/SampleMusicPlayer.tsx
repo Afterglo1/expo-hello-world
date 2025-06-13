1b209ede1e3607f3d9ccd6214a2679938f4cce6c
Shreedhar Hegde <snhkelemane@gmail.com>
2025-06-13 16:25:30 +0530

    feat(recorder): add SampleMusicPlayer component for audio playback
    - Introduced a new SampleMusicPlayer component to manage audio playback with play and stop controls.
    - Implemented UI elements to indicate playback status and enhance user interaction.

---
+++ card-interface/components/recorder/SampleMusicPlayer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Pressable, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+interface SampleMusicPlayerProps {
+  isPlaying: boolean;
+  onPlay: () => void;
+  onStop: () => void;
+}
+
+export const SampleMusicPlayer: React.FC<SampleMusicPlayerProps> = ({
+  isPlaying,
+  onPlay,
+  onStop,
+}) => {
+  return (
+    <View
+      className={`flex-row justify-between items-center p-2 ${
+        isPlaying ? "bg-slate-300 text-white" : ""
+      }`}
+    >
+      <Pressable onPress={onPlay} className="flex-1 h-10 justify-center">
+        <Text> 1. Sample Music</Text>
+      </Pressable>
+      {isPlaying && (
+        <Pressable
+          onPress={onStop}
+          className="w-[20px] h-[20px] rounded-full bg-red-500 justify-center items-center"
+        >
+          <Ionicons name="stop" size={10} color="white" />
+        </Pressable>
+      )}
+    </View>
+  );
+};
