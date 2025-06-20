b924565e52d3ffdbff1a8d65b44f358a02981022
Shreedhar Hegde <snhkelemane@gmail.com>
2025-06-20 16:12:32 +0530

    feat(config): add API_URL configuration and update app.json
    - Introduced app.config.js to manage API_URL from environment variables.
    - Updated app.json to include expo-asset in dependencies.
    - Modified config.ts to utilize the new API_URL for base API requests.

--- card-interface/api/config.ts
+++ card-interface/api/config.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import Constants from "expo-constants";
 
-const BASE_URL = "";
+const BASE_URL = `${Constants.expoConfig?.extra?.API_URL}/api`;
+
 const api = axios.create({
   baseURL: BASE_URL,
 });
